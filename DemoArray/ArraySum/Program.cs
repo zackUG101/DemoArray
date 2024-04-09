d4809946b6b471b48e26a7af7c347b21db914e63
zackeus01 <zackwilson0805@gmail.com>
2024-04-09 19:30:33 +0700

    [feat] Array Sum
    Add Array Sum

---
+++ DemoArray/ArraySum/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers = new int[5];
+        numbers[0] = 2;
+        numbers[1] = 5;
+        numbers[2] = 9;
+        numbers[3] = 6;
+        numbers[4] = 7;
+
+        int total = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            total = total + numbers[i];
+        }
+        Console.WriteLine(total.ToString());
+    }
+}

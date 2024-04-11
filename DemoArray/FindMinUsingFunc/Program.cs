7762b33b27ec1c1930a0e3183d674ad0ce19daca
zackeus01 <zackwilson0805@gmail.com>
2024-04-11 19:43:26 +0700

    [feat] Find Min using function
    Find Min using function

---
+++ DemoArray/FindMinUsingFunc/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    public static void Main(String[] args)
+    {
+        int[] arr = { 4, 12, 7, 8, 1, 6, 9 };
+        int index = MinValue(arr);
+        Console.WriteLine("The smallest element in the array is: " + arr[index]);
+    }
+
+    public static int MinValue(int[] array)
+    {
+
+        int min = array[0];
+        int index = 0;
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < min)
+            {
+                min = array[i];
+                index = i;
+            }
+        }
+
+        return index;
+    }
+
+}

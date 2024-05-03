da324d19044ae8aa880d9e7fa178b521303e6847
zackeus01 <166514124+zackUG101@users.noreply.github.com>
2024-05-03 20:54:28 +0700

    asd
    dasd

---
+++ DemoArray/DiagonalSum/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the size of the square matrix: ");
+        int size = int.Parse(Console.ReadLine());
+        double[,] matrix = new double[size, size];
+
+        Console.WriteLine("Enter the elements of the square matrix:");
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write($"Element [{i},{j}]: ");
+                matrix[i, j] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        double sum = CalculateDiagonalSum(matrix);
+        Console.WriteLine($"The sum of the elements on the main diagonal is: {sum}");
+    }
+
+    static double CalculateDiagonalSum(double[,] matrix)
+    {
+        double sum = 0;
+        int size = matrix.GetLength(0); // Assuming matrix is square
+        for (int i = 0; i < size; i++)
+        {
+            sum += matrix[i, i];
+        }
+        return sum;
+    }
+}
